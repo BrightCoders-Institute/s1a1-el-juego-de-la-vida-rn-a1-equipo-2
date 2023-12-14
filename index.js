7b2c2c70885b4730388779c4da727b93cd2c041f
Daniel Preciado <fpreciado3@ucol.mx>
2023-12-14 17:18:12 -0600

    Fix in index.js and add eslint file
    Co-authored-by: Vanessa-Covarrubias-Nava <Vanessa.covarrubias.nava@gmail.com>
    Co-authored-by: CalixtoSD1117 <ccalixtoengelberger117@gmail.com>
    Co-authored-by: Jherrera11 <jherrera11@ucol.mx>
    Co-authored by: VISOSO2403 <ovisosodev@gmail.com>

--- index.js
+++ index.js
@@ -1,239 +1,103 @@
-var rows = 38;
-var cols = 100;
-
-var playing = false;
-
-var grid = new Array(rows);
-var nextGrid = new Array(rows);
-
-var timer;
-var reproductionTime = 100;
-
-function initializeGrids() {
-  for (var i = 0; i < rows; i++) {
-    grid[i] = new Array(cols);
-    nextGrid[i] = new Array(cols);
+class GameOfLife {
+  constructor(rows, columns) {
+    this.rows = rows;
+    this.columns = columns;
+    this.currentGeneration = this.getRandomInitialState();
   }
-}
 
-function resetGrids() {
-  for (var i = 0; i < rows; i++) {
-    for (var j = 0; j < cols; j++) {
-      grid[i][j] = 0;
-      nextGrid[i][j] = 0;
-    }
-  }
-}
-
-function copyAndResetGrid() {
-  for (var i = 0; i < rows; i++) {
-    for (var j = 0; j < cols; j++) {
-      grid[i][j] = nextGrid[i][j];
-      nextGrid[i][j] = 0;
-    }
-  }
-}
-
-// Initialize
-function initialize() {
-  createTable();
-  initializeGrids();
-  resetGrids();
-  setupControlButtons();
-}
-
-// Lay out the board
-function createTable() {
-  var gridContainer = document.getElementById("gridContainer");
-  if (!gridContainer) {
-    // Throw error
-    console.error("Problem: No div for the drid table!");
-  }
-  var table = document.createElement("table");
-
-  for (var i = 0; i < rows; i++) {
-    var tr = document.createElement("tr");
-    for (var j = 0; j < cols; j++) {
-      //
-      var cell = document.createElement("td");
-      cell.setAttribute("id", i + "_" + j);
-      cell.setAttribute("class", "dead");
-      cell.onclick = cellClickHandler;
-      tr.appendChild(cell);
-    }
-    table.appendChild(tr);
-  }
-  gridContainer.appendChild(table);
-}
-
-function cellClickHandler() {
-  var rowcol = this.id.split("_");
-  var row = rowcol[0];
-  var col = rowcol[1];
-
-  var classes = this.getAttribute("class");
-  if (classes.indexOf("live") > -1) {
-    this.setAttribute("class", "dead");
-    grid[row][col] = 0;
-  } else {
-    this.setAttribute("class", "live");
-    grid[row][col] = 1;
-  }
-}
-
-function updateView() {
-  for (var i = 0; i < rows; i++) {
-    for (var j = 0; j < cols; j++) {
-      var cell = document.getElementById(i + "_" + j);
-      if (grid[i][j] == 0) {
-        cell.setAttribute("class", "dead");
-      } else {
-        cell.setAttribute("class", "live");
+  getRandomInitialState() {
+    const initialState = [];
+    for (let i = 0; i < this.rows; i++) {
+      initialState[i] = [];
+      for (let j = 0; j < this.columns; j++) {
+        // Generar 0 o 1 de manera aleatoria
+        initialState[i][j] = Math.random() < 0.5 ? 0 : 1;
       }
     }
+    return initialState;
   }
-}
 
-function setupControlButtons() {
-  // button to start
-  var startButton = document.getElementById("start");
-  startButton.onclick = startButtonHandler;
+  printBoard(board) {
+    let output = '';
+    for (let i = 0; i < this.rows; i++) {
+      let row = '';
+      for (let j = 0; j < this.columns; j++) {
+        row += board[i][j] ? '■ ' : '□ ';
+      }
+      output += `${row.trim()}\n`;
+    }
+    console.log(output);
+  }
 
-  // button to clear
-  var clearButton = document.getElementById("clear");
-  clearButton.onclick = clearButtonHandler;
-
-  // button to set random initial state
-  var randomButton = document.getElementById("random");
-  randomButton.onclick = randomButtonHandler;
-}
-
-function randomButtonHandler() {
-  if (playing) return;
-  clearButtonHandler();
-  for (var i = 0; i < rows; i++) {
-    for (var j = 0; j < cols; j++) {
-      var isLive = Math.round(Math.random());
-      if (isLive == 1) {
-        var cell = document.getElementById(i + "_" + j);
-        cell.setAttribute("class", "live");
-        grid[i][j] = 1;
+  nextGeneration() {
+    const newGeneration = [];
+    for (let i = 0; i < this.rows; i++) {
+      newGeneration[i] = [];
+      for (let j = 0; j < this.columns; j++) {
+        const neighbors = this.countNeighbors(i, j);
+        if (this.currentGeneration[i][j] === 1) {
+          // Reglas para células vivas
+          newGeneration[i][j] = neighbors === 2 || neighbors === 3 ? 1 : 0;
+        } else {
+          // Reglas para células muertas
+          newGeneration[i][j] = neighbors === 3 ? 1 : 0;
+        }
       }
     }
-  }
-}
-
-// clear the grid
-function clearButtonHandler() {
-  console.log("Clear the game: stop playing, clear the grid");
-
-  playing = false;
-  var startButton = document.getElementById("start");
-  startButton.innerHTML = "Start";
-  clearTimeout(timer);
-
-  var cellsList = document.getElementsByClassName("live");
-  // convert to array first, otherwise, you're working on a live node list
-  // and the update doesn't work!
-  var cells = [];
-  for (var i = 0; i < cellsList.length; i++) {
-    cells.push(cellsList[i]);
+    // Copia la matriz actual a la matriz anterior
+    this.previousGeneration = this.currentGeneration.map((row) => row.slice());
+    // Actualiza la matriz actual con la nueva generación
+    this.currentGeneration = newGeneration;
   }
 
-  for (var i = 0; i < cells.length; i++) {
-    cells[i].setAttribute("class", "dead");
-  }
-  resetGrids;
-}
-
-// start/pause/continue the game
-function startButtonHandler() {
-  if (playing) {
-    console.log("Pause the game");
-    playing = false;
-    this.innerHTML = "Continue";
-    clearTimeout(timer);
-  } else {
-    console.log("Continue the game");
-    playing = true;
-    this.innerHTML = "Pause";
-    play();
-  }
-}
-
-// run the life game
-function play() {
-  computeNextGen();
-
-  if (playing) {
-    timer = setTimeout(play, reproductionTime);
-  }
-}
-
-function computeNextGen() {
-  for (var i = 0; i < rows; i++) {
-    for (var j = 0; j < cols; j++) {
-      applyRules(i, j);
+  countNeighbors(x, y) {
+    let count = 0;
+    for (let i = -1; i <= 1; i++) {
+      for (let j = -1; j <= 1; j++) {
+        const newRow = x + i;
+        const newColumn = y + j;
+        if (
+          newRow >= 0 && newRow < this.rows
+          && newColumn >= 0 && newColumn < this.columns
+          && !(i === 0 && j === 0)
+        ) {
+          count += this.currentGeneration[newRow][newColumn];
+        }
+      }
     }
+    return count;
   }
 
-  // copy NextGrid to grid, and reset nextGrid
-  copyAndResetGrid();
-  // copy all 1 values to "live" in the table
-  updateView();
-}
+  getPreviousGeneration() {
+    return this.previousGeneration;
+  }
 
-// RULES
-// Any live cell with fewer than two live neighbours dies, as if caused by under-population.
-// Any live cell with two or three live neighbours lives on to the next generation.
-// Any live cell with more than three live neighbours dies, as if by overcrowding.
-// Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-
-function applyRules(row, col) {
-  var numNeighbors = countNeighbors(row, col);
-  if (grid[row][col] == 1) {
-    if (numNeighbors < 2) {
-      nextGrid[row][col] = 0;
-    } else if (numNeighbors == 2 || numNeighbors == 3) {
-      nextGrid[row][col] = 1;
-    } else if (numNeighbors > 3) {
-      nextGrid[row][col] = 0;
+  arraysAreEqual(arr1, arr2) {
+    for (let i = 0; i < arr1.length; i++) {
+      for (let j = 0; j < arr1[i].length; j++) {
+        if (arr1[i][j] !== arr2[i][j]) {
+          return false;
+        }
+      }
     }
-  } else if (grid[row][col] == 0) {
-    if (numNeighbors == 3) {
-      nextGrid[row][col] = 1;
-    }
+    return true;
   }
 }
 
-function countNeighbors(row, col) {
-  var count = 0;
-  if (row - 1 >= 0) {
-    if (grid[row - 1][col] == 1) count++;
-  }
-  if (row - 1 >= 0 && col - 1 >= 0) {
-    if (grid[row - 1][col - 1] == 1) count++;
-  }
-  if (row - 1 >= 0 && col + 1 < cols) {
-    if (grid[row - 1][col + 1] == 1) count++;
-  }
-  if (col - 1 >= 0) {
-    if (grid[row][col - 1] == 1) count++;
-  }
-  if (col + 1 < cols) {
-    if (grid[row][col + 1] == 1) count++;
-  }
-  if (row + 1 < rows) {
-    if (grid[row + 1][col] == 1) count++;
-  }
-  if (row + 1 < rows && col - 1 >= 0) {
-    if (grid[row + 1][col - 1] == 1) count++;
-  }
-  if (row + 1 < rows && col + 1 < cols) {
-    if (grid[row + 1][col + 1] == 1) count++;
-  }
-  return count;
-}
+const game = new GameOfLife(10, 15);
+console.log('Primera Generación:');
+game.printBoard(game.currentGeneration);
+let generationCount = 1;
 
-// Start everything
-window.onload = initialize;
+while (typeof neverDeclared === "undefined") {
+  game.nextGeneration();
+  console.log(`Generación ${generationCount}:`);
+  game.printBoard(game.currentGeneration);
+
+  // Compara la generación actual con la anterior
+  if (game.arraysAreEqual(game.currentGeneration, game.getPreviousGeneration())) {
+    console.log('El juego se ha estabilizado. No se pueden hacer más generaciones.');
+    break;
+  }
+  generationCount++;
+}
